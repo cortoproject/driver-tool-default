ca360309faad5870713b31c30dea51525d4a3fe6
Sander Mertens <sander.mertens8@gmail.com>
2017-12-09 17:09:01 -0800

    Change to v2.0

--- include/default.h
+++ include/default.h
@@ -1,18 +1,23 @@
+/* default.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
 
 #ifndef DRIVER_TOOL_DEFAULT_H
 #define DRIVER_TOOL_DEFAULT_H
 
-/* Add include files here */
+#include <corto/corto.h>
+#include <driver/tool/default/_project.h>
+#include <corto/argparse/argparse.h>
 
-#ifdef __cplusplus
-extern "C" {
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
 #endif
-
-/* Insert definitions here */
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* DRIVER_TOOL_DEFAULT_H */
 
--- src/default.c
+++ src/default.c
@@ -4,30 +4,26 @@
 
 int cortomain(int argc, char *argv[]) {
 
-    if ((corto_fileTest("rakefile") && !corto_isdir("rakefile")) || corto_fileTest("project.json")) {
-        corto_run("driver/tool/build", 1, (char*[]){"build", NULL});
-    } else {
-        int8_t rc, sig = corto_proccmd("corto --logo", &rc);
-        if (sig || rc) {
-            corto_seterr("cannot invoke corto tool");
-            goto error;
-        }
-        printf("Welcome to Corto! Unsure about what to do? Here are some ideas:\n");
-        printf("\n");
-        printf("corto create myApp\n");
-        printf("  Create a new corto app.\n");
-        printf("\n");
-        printf("corto create package myPackage\n");
-        printf("  Create a new corto package.\n");
-        printf("\n");
-        printf("corto sh\n");
-        printf("  Start the corto shell.\n");
-        printf("\n");
-        printf("corto --help\n");
-        printf("  Show all available corto commands.\n");
-        printf("\n");
-        printf("Happy coding!\n");
+    int8_t rc, sig = corto_proc_cmd("corto --logo", &rc);
+    if (sig || rc) {
+        corto_throw("cannot invoke corto tool");
+        goto error;
     }
+    printf("Welcome to Corto! Unsure about what to do? Here are some ideas:\n");
+    printf("\n");
+    printf("corto create myApp\n");
+    printf("  Create an executable corto application.\n");
+    printf("\n");
+    printf("corto create package myPackage\n");
+    printf("  Create a corto package.\n");
+    printf("\n");
+    printf("corto sh\n");
+    printf("  Start the corto shell.\n");
+    printf("\n");
+    printf("corto --help\n");
+    printf("  Show all available corto commands.\n");
+    printf("\n");
+    printf("Happy coding!\n");
 
     return 0;
 error:
